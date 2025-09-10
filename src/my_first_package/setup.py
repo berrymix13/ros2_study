95bcd0dfa25faf822472e74a3a677e392fce935b
berrymix13 <sujini7773@gmail.com>
2025-09-10 09:20:57 +0900

    chore: Add launch files to package installation

--- src/my_first_package/setup.py
+++ src/my_first_package/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+import os, glob
 
 package_name = 'my_first_package'
 
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name+'/launch', glob.glob(os.path.join("launch", "*.launch.*"))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
